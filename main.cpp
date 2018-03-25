0f7bcbff2065e57de2c32f700f2a61a608c294a7
Vova <kekeev@mail.ua>
2018-03-25 20:06:54 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "std.h"
+#include<locale.h>
+
+using namespace std;
+
+int main()
+{
+}
---
+++ std.h
@@ -0,0 +1,14 @@
+#ifndef ADD_H
+#define ADD_H
+ 
+
+int enter(int a,int b)
+{
+	cout << "Введите первое число: ";
+	cin >> a;
+	cout << "Введите второе число: ";
+	cin >> b;
+	return a+b;
+}
+ 
+#endif
